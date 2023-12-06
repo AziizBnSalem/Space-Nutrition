2eb374094639fca9f36d53054389311c3de81994
Aziiz <mohamedaziz.bensalem@gmail.com>
2023-12-06 11:35:49 +0100

    Php modify for a login page ..

--- Lib/nav.php
+++ Lib/nav.php
@@ -2,4 +2,6 @@
     <li><a href="./index.php">Products</a></li>
     <li><a href="./Home.php">Home</a></li>
     <li><a href="./contact.php">Contact</a></li>
+    <li><a href="./login.php">Logout</a></li>
+
 </ul>
